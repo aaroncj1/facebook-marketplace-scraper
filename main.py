1c9a166186c2ea75a6a067b13a218c12a6ddc08a
unknown <aaroncj@protonmail.com>
2023-04-12 20:37:37 -0500

    updated xpaths

--- main.py
+++ main.py
@@ -21,18 +21,20 @@ def main():
 
     for key in range(len(user.user["facebook.marketplace.keywords"])):
         print("Searching for " + user.user["facebook.marketplace.keywords"][key])
-        search = driver.find_element_by_xpath("//*[@aria-label='Search Marketplace']")
+        search = driver.find_element("xpath", "//*[@aria-label='Search Marketplace']")
+        search.send_keys(Keys.CONTROL + "a")
+        search.send_keys(Keys.DELETE)
         search.send_keys(user.user["facebook.marketplace.keywords"][key])
         search.send_keys(Keys.RETURN)
         time.sleep(randrange(1,8))
-        for title in driver.find_elements_by_xpath("//*[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7'][@style='-webkit-box-orient: vertical; -webkit-line-clamp: 2; display: -webkit-box;']"):
+        for title in driver.find_elements("xpath", "//*[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6'][@style='-webkit-box-orient: vertical; -webkit-line-clamp: 2; display: -webkit-box;']"):
             listings["keyword"].append(user.user["facebook.marketplace.keywords"][key])
             listings["title"].append(str(title.text))
-        for price in driver.find_elements_by_xpath(".//*[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d3f4x2em fe6kdd0r mau55g9w c8b282yb mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id'][@dir='auto']"):
+        for price in driver.find_elements("xpath", './/*[@class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u"][@dir="auto"]'):
             listings["price"].append(str(price.text))
-        for url in driver.find_elements_by_xpath('.//a[contains(@href,"/marketplace/item")][@class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 p8dawk7l"]'):
+        for url in driver.find_elements("xpath", './/a[contains(@href,"/marketplace/item")][@class="x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv"]'):
             listings["url"].append(url.get_attribute("href"))
-        for location in driver.find_elements_by_xpath(".//*[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5 ojkyduve'][string-length(text()) > 0]"):
+        for location in driver.find_elements("xpath", ".//*[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft x1j85h84'][string-length(text()) > 0]"):
             listings["location"].append(str(location.text))
         # driver.quit()
     print(str(listings))
@@ -51,12 +53,13 @@ def main():
 
 def signin():
     print("Signing in")
-    email_box = driver.find_element_by_name("email")
+    email_box = driver.find_element("name", "email")
     email_box.send_keys(user.user["facebook.email"])
-    password_box = driver.find_element_by_name("pass")
+    password_box = driver.find_element("name", "pass")
     password_box.send_keys(user.user["facebook.password"])
     password_box.send_keys(Keys.RETURN)
     time.sleep(randrange(3, 6))
+    driver.get("https://www.facebook.com/marketplace")
     main()
     print("Signed in")
 
@@ -65,10 +68,10 @@ if __name__ == "__main__":
     print("!!! Due to the nature of how Facebook handles web scraping, the sleep times between pages randomly changes to throw off Facebook.")
     PATH = "chromedriver.exe"
     options = Options()
-    options.add_argument('--headless')
+ #   options.add_argument('--headless')
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(PATH, options=options)
-    driver.get("https://www.facebook.com/marketplace/")
+    driver.get("https://www.facebook.com")
     print("Connected to Facebook Marketplace")
     signin()
